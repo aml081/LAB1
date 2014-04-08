26ae45d2a14ca9184488d796374a0f62d879b3f0
Ann <aml081@ucsd.edu>
2014-04-08 09:49:21 -0700

    Oops, we didn't want this commit

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello
 {
 	public static void man (String argv[])
 	{
-	//Default is "World"
+	//This is an unwanted but staged comment
 	String name = "World";
 	if (argv.length != 0)
 	{
